aacb90061db4adfeef3a4b56a9bc44d1af80f737
RabeeaFatima14 <23-22411-049@se.fjwu.edu.pk>
2025-10-19 06:52:51 +0000

    Add portfolio site for GitHub Pages

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple fade-in effect
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.style.opacity = 0;
+  setTimeout(() => (document.body.style.transition = "opacity 1.5s ease-in"), 0);
+  setTimeout(() => (document.body.style.opacity = 1), 100);
+});
